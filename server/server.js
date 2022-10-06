b66f4f4832ea53362926da608df4dc61a2ea776c
dmadon <deanna.madon@gmail.com>
2022-10-05 20:18:02 -0500

    minor bug fixes in src-sw.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,11 @@
 const express = require('express');
-// const mongoose = require('mongoose');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+require('./routes/htmlRoutes')(app);
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/text-editor',{});
-// mongoose.set('debug',true);
-
-require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`🌍 Now connected on port: ${PORT}`));
